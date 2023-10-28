78c83eab3969fba7a071135d31f52c451325fe13
Martin Rüegg <martin.rueegg@metaworx.ch>
2023-10-28 17:34:19 +0200

    Merge source v0.20.1/6ab6c0a923800ead877d5f222a51f5f4236d889a

--- protected/humhub/modules/file/widgets/ShowFiles.php
+++ protected/humhub/modules/file/widgets/ShowFiles.php
@@ -2,6 +2,7 @@
 
 namespace humhub\modules\file\widgets;
 
+use humhub\modules\content\components\ContentActiveRecord;
 use humhub\models\Setting;
 
 /**
@@ -23,6 +24,15 @@ class ShowFiles extends \yii\base\Widget
      */
     public function run()
     {
+        if ($this->object instanceof ContentActiveRecord) {
+            $widget = $this->object->getWallEntryWidget();
+
+            // File widget disabled in this wall entry
+            if ($widget->showFiles === false) {
+                return;
+            }
+        }
+
         $blacklisted_objects = explode(',', Setting::GetText('showFilesWidgetBlacklist', 'file'));
         if (!in_array(get_class($this->object), $blacklisted_objects)) {
             $files = \humhub\modules\file\models\File::getFilesOfObject($this->object);
