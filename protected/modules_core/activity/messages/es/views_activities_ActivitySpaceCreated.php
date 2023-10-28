f40d6ab74bec68a06037a82aa0014f61e9cf9c01
Martin Rüegg <martin.rueegg@metaworx.ch>
2023-10-28 17:30:46 +0200

    Merge source v0.9.0-rc.1/94479c5f5ba9c1bc39e19d381464509d22e65402

---
+++ protected/modules_core/activity/messages/es/views_activities_ActivitySpaceCreated.php
@@ -0,0 +1,5 @@
+<?php
+return array (
+  '%displayName% created the new space %spaceName%' => '%displayName% creo el nuevo espacio %spaceName%',
+  '%displayName% created this space.' => '%displayName% creo este espacio.',
+);
---
+++ protected/modules_core/admin/messages/pt_br/controllers_UserController.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'Become this user' => 'Torne-se este usuário',
+  'Delete' => 'Apagar',
+  'Disabled' => 'Desabilitado',
+  'Enabled' => 'Habilitado',
+  'Save' => 'Salvar',
+  'Unapproved' => 'Não aprovado',
+  'User not found!' => 'Usuário não encontrado!',
+);
